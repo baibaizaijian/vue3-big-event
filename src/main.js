a442e574918eee188d5882dee8b33765116f0b32
白熊想吃鱼 <3024131925@qq.com>
2023-10-14 21:14:35 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/assets/main.scss'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-// console.log(abc)
 
 app.use(createPinia())
 app.use(router)
